d8f7a5b936273dcacc2c774312ed392f8539d11c
Oleg Krechkovskiy <olegkrechk@gmail.com>
2023-09-08 20:24:46 +0300

    [OK-theme-switcher] Добавил компонент переключатель темы

--- src/components/layout/Header/HeaderTop/HeaderTop.jsx
+++ src/components/layout/Header/HeaderTop/HeaderTop.jsx
@@ -1,43 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './HeaderTop.module.scss';
-import { ReactComponent as AutoImg } from '../../../../assets/images/sunMoon.svg';
+import ThemeSwitcher from 'components/themeSwitcher/ThemeSwitcher';
 
 const HeaderTop = () => {
-  const themeLocal = localStorage.getItem('themeLocal') || 0;
-  let systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
-  let themeMode = themeLocal ? themeLocal : systemTheme;
-  let root = document.querySelector(':root');
-
-  const btnRef = useRef(null);
-  const btnAuto = useRef(null);
-  const [themMode, setThemModeMode] = useState(themeMode);
-
-  useEffect(() => {
-    root.classList = `${themMode}`;
-    btnRef.current.dataset.theme = `${themMode}`;
-    btnAuto.current.dataset.theme = `${themMode}`;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [themMode]);
-
-  const toggleTheme = e => {
-    e.stopPropagation();
-    setThemModeMode(currentValue => {
-      let them = currentValue === 'light' ? 'dark' : 'light';
-      localStorage.setItem('themeLocal', them);
-      return them;
-    });
-  };
-  const setAuto = () => {
-    let systemTheme = themeMode.matches ? 'dark' : 'light';
-    localStorage.removeItem('themeLocal');
-    setThemModeMode(`${systemTheme}`);
-    btnRef.current.dataset.theme = `${systemTheme}`;
-  };
-
   return (
     <div className={styles.wrapper}>
       <div className='container'>
@@ -58,24 +25,7 @@ const HeaderTop = () => {
               +79891233232
             </a>
           </div>
-          <div className={styles.toggle}>
-            <div
-              ref={btnRef}
-              className={styles.theme}
-              title='дневная/ночная темы'
-              data-theme=''
-              onClick={toggleTheme}
-            ></div>
-            <div
-              ref={btnAuto}
-              onClick={setAuto}
-              className={styles.auto}
-              data-theme=''
-              title='Использовать настройки системы'
-            >
-              <AutoImg />
-            </div>
-          </div>
+          <ThemeSwitcher />
         </div>
       </div>
     </div>
