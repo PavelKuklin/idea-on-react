54c168fedd050f3d4bb8321e2154f72eff14be1c
Oleg Krechkovskiy <olegkrechk@gmail.com>
2025-06-28 13:08:37 +0300

    Переписывам на vite

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 - https://resplendent-creponne-cc2d35.netlify.app/
 - https://github.com/PavelKuklin/idea-on-react/tree/main
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Footer from 'components/layout/Footer/Footer';
-import Home from 'pages/Home';
-import NotFound from 'pages/NotFound/NotFound';
-import Catalog from 'pages/Collection/Collection';
-import Header from 'components/layout/Header/Header';
-import Favorites from 'pages/Favorites/Favorites';
-import UserAccount from 'pages/UserAccount/UserAccount';
-import ClampFunction from 'pages/ClampFunction/ClampFunction';
-import ToTopButton from 'components/buttonToTop/ToTopButton';
+function App() {
+  const [count, setCount] = useState(0)
 
-
-function app() {
   return (
-    <div className='main-wrapper'>
-      <Header />
-
-      <div className='content'>
-        {/* // связываем адреса и компоненты (грубо говоря) */}
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/user-account' element={<UserAccount />} />
-          <Route path='/favorites' element={<Favorites />} />
-          <Route path='/collection/all' element={<Catalog />} />
-          <Route path='/clamp-function' element={<ClampFunction />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <ToTopButton />
-      <Footer />
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default app;
+export default App
